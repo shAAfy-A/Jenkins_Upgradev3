8e9b60bb1dee78bfa6526bf86b3f603dbfec3926
shAAfy-A <ahmed.abdelshafy12@outlook.com>
2025-01-28 17:12:23 +0200

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -26,6 +26,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
+
+   @Test
+  public void intentional_failure(){
+    assertTrue(false);
+  }
   
 
   @Test
